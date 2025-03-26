abec08f82f29c87cacb00bf66ce9cbff8bb9c663
Harsh Pandey <harsh@Harshs-MacBook-Air.local>
2025-03-26 22:57:45 +0530

    minor changes

--- notificationService/src/main/java/com/notificationService/NotificationServiceApplication.java
+++ notificationService/src/main/java/com/notificationService/NotificationServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Inside notification");
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
