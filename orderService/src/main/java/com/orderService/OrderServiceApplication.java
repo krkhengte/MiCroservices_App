4d212ca307032437f726579322e93b28b3c19b4e
krkhengate123@gmail.com <krkhengate123@gmail.com>
2025-03-26 22:46:05 +0530

    msg added

--- orderService/src/main/java/com/orderService/OrderServiceApplication.java
+++ orderService/src/main/java/com/orderService/OrderServiceApplication.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("welcome");
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
